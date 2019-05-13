111cb483fa5f1d28d200fb05ff486f00d20c47ec
sunilregmi233 <sunilregmi223@gmail.com>
2019-05-13 20:35:43 +0545

    bootstrap and password change

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -38,7 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'pages',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
